c069d97aaa9508c1536a2d2b894e636f16ed8f3d
Kouson Nakajima <winnie.2011@icloud.com>
2020-03-17 14:55:37 +0900

    Add files via upload

---
+++ Main001ver2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int a = Integer.parseInt(sc.next());
+		int b = Integer.parseInt(sc.next());
+		int c = Integer.parseInt(sc.next());
+		String s = Integer.parse(sc.next());
+		System.out.println((a+b+c) + " " + s);
+	}
+}
